a234372874e247bb09c8d7b07b18e70edb71ddbb
Nathan Long <him@nathanmlong.com>
2017-04-28 17:33:12 -0400

    Note to self

--- web/controllers/call_controller.ex
+++ web/controllers/call_controller.ex
@@ -2,10 +2,11 @@ defmodule Phonerwall.CallController do
   use Phonerwall.Web, :controller
 
   def incoming(conn, _params) do
+    # TODO - set an XML cotent-type, see if that helps get it to record
     text conn, """
       <Response>
           <Say voice="woman">Flimsy human, state your purpose!</Say>
-          <Record maxLength="20" />
+          <Record playBeep="true" maxLength="20" />
       </Response>
     """
   end
